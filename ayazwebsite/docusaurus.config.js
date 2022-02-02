cd2d856c4c1a147b3a9e9f982392b9c4e9f14245
Ayaz Zafar <85080816+AyazZafar@users.noreply.github.com>
2022-02-02 10:50:53 +0000

    Update docusaurus.config.js

--- ayazwebsite/docusaurus.config.js
+++ ayazwebsite/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'AYAZ ZAFAR',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'WEB DEVELOPER, IT TECHNICIAN & PHOTOGRAPHER',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
